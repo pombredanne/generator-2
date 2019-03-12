3914462ab8810a4a1de44c4dd19540401a12d693
Kay Lin <kaymw@aosc.io>
2019-03-11 22:02:20 -0700

    generate.py: Some comments and some adjustments
    Signed-off-by: Kay Lin <kaymw@aosc.io>

--- generate.py
+++ generate.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+"""
+Generates autobuild files from template
+"""
+
 import os
 import json
 import shutil
@@ -17,10 +21,13 @@ KEYS_NECESSARY = ['name', 'deps']
 VERSION_LOG = 'versions.json'
 REPOLOGY_ENDPOINT = 'https://repology.org/api/v1/project/%s'
 
-VERSIONS = dict()
-
 
 def check_version(version):
+    """
+    Get correct version from the source definition
+    :param version: Version part of the source definition
+    :return: The version of the package generated from the source file
+    """
     if version['method'] == 'static':
         return version['static']
     elif version['method'] == 'repology':
@@ -29,7 +36,7 @@ def check_version(version):
         if 'distro' in version.keys():
             distro = version['distro']
         else:
-            distro = 'debian_testing'
+            distro = 'debian_testing'  # Follow Debiantai testing as default
         latest_ver = ''
         for package in api_response:
             if package['repo'] == distro:
@@ -44,8 +51,15 @@ def check_version(version):
         return '9999'
 
 
-def autobuild_generate(src_file):
-    content = json.loads(src_file.read())
+def autobuild_generate(src_file, versions):
+    """
+    Generate autobuild files from template
+
+    :param src_file: Source file in spiral format
+    :param versions: Version logs
+    :return: Version logs
+    """
+    content = json.loads(src_file)
     for key in KEYS_NECESSARY:
         if key not in content.keys():
             raise ValueError('field  \'%s\' is required')
@@ -56,11 +70,11 @@ def autobuild_generate(src_file):
     pkg_ver = '9999'
     if 'version' in content.keys():
         pkg_ver = check_version(content['version'])
-    if pkg_name in VERSIONS.keys():
-        if pkg_ver == VERSIONS[pkg_name]:
-            logger.info('%s: no updates found, ignoring...')
-            return
-    VERSIONS[pkg_name] = pkg_ver
+    if pkg_name in versions.keys():
+        if pkg_ver == versions[pkg_name]:
+            logger.info('%s: no updates found, ignoring...' % pkg_name)
+            return versions
+    versions[pkg_name] = pkg_ver
     dest = OUTPUT_ABBS + '/extra-spiral/' + content['name']
     if os.path.exists(dest):
         shutil.rmtree(dest)
@@ -76,10 +90,17 @@ def autobuild_generate(src_file):
             dest_file_content = dest_file_content.replace('@VER@', str(pkg_ver))
             with open(dest_file_name, 'w') as f:
                 f.write(dest_file_content)
-    return
+    return versions
 
 
-def generate(repo_location):
+def generate(repo_location, versions):
+    """
+    Generate autobuild tree from given spiral format repo
+
+    :param repo_location: The location of the repository
+    :param versions: Version log
+    :return: None
+    """
     if not os.path.exists(TEMPLATE_DIR):
         logger.error('Template directory does not exist, quitting...')
         exit(1)
@@ -89,22 +110,39 @@ def generate(repo_location):
         for name in (files + subdirs):
             with open(path + name) as f:
                 try:
-                    autobuild_generate(f)
-                    logger.info('%s prepared' % name)
+                    versions = autobuild_generate(f.read(), versions)
+                    logger.info('%s: prepared' % name)
                 except ValueError as e:
-                    logger.error('%s omitted, %s' % (name, e))
+                    logger.error('%s: omitted, %s' % (name, e))
                     continue
+    return versions
+
+
+def start():
+    """
+    Prepare for file generation
+
+    :return: None
+    """
+    if os.path.exists(VERSION_LOG):
+        with open(VERSION_LOG) as f:
+            try:
+                versions = json.loads(f.read())
+            except ValueError:
+                logger.error('Invalid version log, ignoring...')
+                versions = dict()
+    else:
+        versions = dict()
+    logger.info('>>> Generation start')
+    versions = generate('repo/packages/', versions)
+    logger.info('>>> Generation finished')
+    logger.info('>>> Saving version log...')
+    with open(VERSION_LOG, 'w') as f:
+        f.write(json.dumps(versions, ensure_ascii=False))
+    logger.info('>>> All done, execute build.sh to start building packages')
+    return
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    if os.path.exists(VERSION_LOG):
-        with open(VERSION_LOG) as f:
-            try:
-                VERSIONS = json.loads(f.read())
-            except ValueError:
-                logger.error('Invalid version cache, ignoring...')
-                VERSIONS = dict()
-    generate('repo/packages/')
-    with open(VERSION_LOG, 'w') as f:
-        f.write(json.dumps(VERSIONS, ensure_ascii=False))
+    start()

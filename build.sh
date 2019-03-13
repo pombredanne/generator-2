8c607f55a667790fd069e6ecd8c8d770fb455fe0
Kay Lin <kaymw@aosc.io>
2019-03-12 21:09:34 -0700

    s0Me Sma11 ADjUstmEnts
    Signed-off-by: Kay Lin <kaymw@aosc.io>

--- build.sh
+++ build.sh
@@ -2,23 +2,37 @@
 
 set -e;
 
-REPO="TREE";
+REPO="TREE"; # Path to generated autobuild definitions
+CATEGORY="extra-spiral"; # The category of packages that will be built
+CIEL="/usr/local/bin/ciel"; # Path to ciel binary
+
+if [ ! -f "${CIEL}" ]; then
+	echo ">>> Ciel not found, quitting...";
+	exit 127;
+fi
+
+if [ ! -d "${REPO}/${CATEGORY}" ]; then
+	echo ">>> Autobuild files not found, quitting...";
+	exit 1
+fi
 
 # Upgrade container manually, since ciel update-os is broken right now
-echo ">>> Updaing container"
-sudo ciel add update;
-sudo ciel shell -i update "apt update && apt dist-upgrade -y";
-sudo ciel commit -i update;
-sudo ciel del update;
+echo ">>> Updating container";
+sudo ${CIEL} add update;
+sudo ${CIEL} shell -i update "apt update && apt dist-upgrade -y";
+sudo ${CIEL} commit -i update;
+sudo ${CIEL} del update;
+echo ">>> Container updated";
 
-set +e;
+# Build packages
+set +e; # Since it may fail...
 
-echo ">>> Start building..."
-for package in $(ls "$REPO"/extra-spiral); do
-    echo ">>> Building $package"
-    sudo ciel add $package;
-    sudo ciel build -i $package $package;
-    sudo ciel del $package;
-    echo ">>> Build finished"
+echo ">>> Start building packages..."
+for package in $(ls "${REPO}/${CATEGORY}"); do
+    echo ">>> Building ${package}";
+    sudo ${CIEL} add ${package};
+    sudo ${CIEL} build -i ${package} ${package};
+    sudo ${CIEL} del ${package};
+    echo ">>> Finished building ${package}";
 done
-echo ">>> All done."
+echo ">>> Finished building packages"
--- on_push.sh
+++ on_push.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
+# On push event - Trigger the build process
+
 set -e;
 
-pushd repo
-git pull
-popd
+GIT="/usr/bin/git";
+PIP="venv/bin/pip3";
+TREE="TREE"
+PYTHON="venv/bin/python3";
+GENERATOR="generator.py";
+BUILD_SCRIPT="./build.sh";
 
-sudo rm -Rf TREE
-venv/bin/python3 generate.py
-bash -c ./build.sh
+REPO="repo";
+
+if [[ ! -f "${GIT}" && ! -f "${PIP}" && ! -f "${PYTHON}" && ! -f "${GENERATOR}" ]]; then
+	echo ">>> Required file/program not found, quitting...";
+fi
+
+# Pull the latest source
+echo ">>> Fetching latest generator...";
+"${GIT}" pull;
+
+# Update the source definitions
+echo ">>> Updating source files...";
+pushd "${REPO}";
+	"${GIT}" pull;
+popd;
+
+# Update python dependencies
+echo ">>> Updating Python dependencies...";
+"${PIP}" install --upgrade -r requirements.txt;
+echo ">>> Dependencies updated"
+
+# Building!
+echo ">>> Starting build process...";
+sudo rm -Rf "${TREE}"; # Since packages are built with root account
+"${PYTHON}" "${GENERATOR}"; # Generate autobuild files
+bash -c "${BUILD_SCRIPT}"; # Build packages from autobuild definitions
+echo ">>> Done!"
